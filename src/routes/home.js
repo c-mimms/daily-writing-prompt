31a1a2df7005a44ea2c384310cc7d2ba8f2d1d53
Chris <christek13@gmail.com>
2023-11-03 22:39:00 +0000

    Add posting function and list of posts to home page.

---
+++ src/routes/home.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { getPost, getPosts } from '../db/posts.js';
+
+const router = Router();
+
+router.get('/', getHomePageHandler);
+
+async function getHomePageHandler(req, res) {
+
+  //Load a page of posts and show in reverse chronological order
+  try {
+    const posts = await getPosts();
+    
+    res.render('home', { posts });
+
+  } catch (error) {
+    console.error('Error retrieving post:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving the post.' });
+  }
+}
+
+export { router };
