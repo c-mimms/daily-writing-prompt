6bfed93a84f67ae96967391ae7e4cf6d85084bba
Chris <christek13@gmail.com>
2023-12-08 00:12:21 -0500

    Add GPT library and a random page generator for testing it. (#14)

---
+++ src/routes/generated.js
@@ -0,0 +1,65 @@
+import { streamGpt } from '../services/gpt.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Create an in memory map to cache wildcardResponsesr
+//Attempt to load from disk first
+const mapPath = path.join(__dirname, 'wildcardResponses.json');
+let wildcardResponses;
+if (fs.existsSync(mapPath)) {
+    const mapJson = fs.readFileSync(mapPath);
+    const mapArray = JSON.parse(mapJson);
+    wildcardResponses = new Map(mapArray);
+} else {
+    wildcardResponses = new Map();
+}
+
+//Calls gpt to handle wildcard routes and generate content using streamGpt
+function wildcardHandler(req, res) {
+  //Check if url is in map
+  if (wildcardResponses.has(req.originalUrl)) {
+    //If so, return the cached response
+    const page = wildcardResponses.get(req.originalUrl);
+    res.send(page);
+    console.log(page)
+    return;
+  }
+
+  const prompt = {
+    'role': 'system',
+    'content': `For the page hosted at /page/${req.params[0]}, start by generating an HTML comment that outlines a context for the page. This backstory should serve as the foundation for the page's theme, style, and content. Do not use placeholders anywhere, all generated content must be unique and interesting. Do not write TODOs or comments in the generated HTML, all content should be final and all javascript should be inlined and completed. 
+Then generate the full HTML for the page, using inline CSS and JS as required. Adapt the content, layout and CSS to fit the theme suggested by the generated page context. 
+All images must be embedded SVGs or wikimedia embeds, and must align with the page's theme if included. Ensure the page is responsive, beautiful, and contains intuitive navigation. All internal links must be relative and confined within the /page/ subpath.`
+  };
+  
+  const messages = [prompt];
+  //Send mime type to browser and keep connection alive with a 5 second timeout
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Connection': 'keep-alive',
+    'Keep-Alive': 'timeout=5',
+  });
+  const callback = (message) => {
+    if (message) {
+      let currentResponse = wildcardResponses.get(req.originalUrl);
+      if (currentResponse === undefined) {
+        currentResponse = '';
+      }
+      //Also cache the streamed response in in-memory map
+      wildcardResponses.set(req.originalUrl, currentResponse + message);
+      res.write(message);
+    } else {
+      res.end();
+      //Save in-memory map to disk
+      const mapJson = JSON.stringify([...wildcardResponses]);
+      fs.writeFileSync(mapPath, mapJson);
+      
+    }
+  }
+  streamGpt(messages, callback);
+}
+
+export { wildcardHandler };
---
+++ src/services/gpt.js
@@ -0,0 +1,115 @@
+import 'node-fetch';
+import { createParser } from 'eventsource-parser';
+
+const completion_endpoint = 'https://api.openai.com/v1/chat/completions';
+const embeddings_endpoint = 'https://api.openai.com/v1/embeddings';
+
+/**
+ * Embed a string of text
+ * @param {string} text - The text to embed
+ * @returns {Promise<object|null>} - A Promise that resolves to the embedding object or null if not found.
+ */
+async function embed(text) {
+    const params = { input: text, model: 'text-embedding-ada-002' };
+    const headers = {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+        'Content-Type': 'application/json',
+    };
+
+    const response = await fetch(embeddings_endpoint, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(params),
+    });
+
+    const json = await response.json();
+    return json;
+}
+
+/**
+ * Query the GPT Chat API
+ * @param {string[]} messages - An array of messages to send to the API
+ * @param {string} stopSequence - The sequence of characters to stop the completion at
+ * @returns {Promise<string>} - A Promise that resolves to the chat response
+ */
+async function queryGpt(messages, stopSequence) {
+  var model = 'gpt-3.5-turbo';
+  var stop = stopSequence;
+  // var model = 'gpt-4';
+  var temperature = 0.7;
+  var maxTokens = 1000;
+
+  const params = { model, messages, stop, temperature, max_tokens: maxTokens };
+  const headers = {
+    Authorization: `Bearer ${process.env.API_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  const response = await fetch(completion_endpoint, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(params),
+  });
+  console.log('Response json: ', response);
+
+  const json = await response.json();
+
+  if (json.choices && json.choices[0] && json.choices[0].message) {
+    return json.choices[0].message.content;
+  } else {
+    console.log('Error: Unexpected API response format', json);
+    return 'ERROR';
+  }
+}
+
+/**
+ * Stream the Chat Completions API
+ * @param {string[]} messages - An array of messages to send to the API
+ * @param {function} callback - A callback function to run on each message
+ */
+async function streamGpt(messages, callback) {
+  let response = await fetch(completion_endpoint,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.API_KEY}`
+      },
+      method: "POST",
+      body: JSON.stringify({
+        // model: "gpt-4",
+        model: 'gpt-3.5-turbo-1106',
+        // model: 'gpt-3.5-turbo',
+        messages: messages,
+        temperature: 0.9,
+        max_tokens: 4000,
+        stream: true,
+      }),
+    });
+
+  console.log("Streaming gpt");
+
+
+  const parser = createParser(thing => {
+    var piece = onParse(thing);
+    if (piece) {
+      console.log(piece);
+      callback(piece);
+    }
+  });
+  for await (const value of response.body?.pipeThrough(new TextDecoderStream())) {
+    parser.feed(value);
+  }
+  callback(null); //Callback with null to indicate that the stream has ended
+}
+
+function onParse(event) {
+  if (event.type === 'event') {
+    if (event.data !== "[DONE]") {
+      return JSON.parse(event.data).choices[0].delta?.content || "";
+    }
+  } else if (event.type === 'reconnect-interval') {
+    console.log('We should set reconnect interval to %d milliseconds', event.value)
+  }
+}
+
+export { queryGpt, streamGpt, embed };
